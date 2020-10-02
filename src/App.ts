6206a6ba4e016c86fbada85d3bbd338a97f4e814
cbusby <callie.a.busby@accenture.com>
2020-10-02 12:55:07 -0400

    Demo app but nothing displays

---
+++ src/App.ts
@@ -0,0 +1,69 @@
+import './App.css';
+import * as SRD from '@projectstorm/react-diagrams';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
+
+
+
+
+export class App {
+  activeModel: SRD.DiagramModel;
+  diagramEngine: SRD.DiagramEngine;
+
+  constructor() {
+    this.diagramEngine = SRD.default();
+    this.activeModel = new SRD.DiagramModel();
+    this.newModel();
+  }
+
+  public newModel() {
+    // this.activeModel = new SRD.DiagramModel();
+    this.diagramEngine.setModel(this.activeModel);
+
+    //3-A) create a default node
+    var node1 = new SRD.DefaultNodeModel('Node 1', 'rgb(0,192,255)');
+    let port = node1.addOutPort('Out');
+    node1.setPosition(100, 100);
+
+    //3-B) create another default node
+    var node2 = new SRD.DefaultNodeModel('Node 2', 'rgb(192,255,0)');
+    let port2 = node2.addInPort('In');
+    node2.setPosition(400, 100);
+
+    // link the ports
+    let link1 = port.link(port2);
+
+    this.activeModel.addAll(node1, node2, link1);
+  }
+
+  public getActiveDiagram(): SRD.DiagramModel {
+    return this.activeModel;
+  }
+
+  public getDiagramEngine(): SRD.DiagramEngine {
+    return this.diagramEngine;
+  }
+}
+
+// export default App;
